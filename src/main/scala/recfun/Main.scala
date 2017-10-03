72d6b83c501eb3440b85db69139f474465ae75a2
Fernando Heleno <feheleno@gmail.com>
2017-10-03 18:21:32 -0300

    adicionadas as funções pascal e balance

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -13,12 +13,27 @@ object Main {
   /**
     * Exercise 1
     */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1
+    else pascal(c, r - 1) + pascal(c - 1, r - 1)
+  }
 
   /**
     * Exercise 2
     */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def maintainControl(chars: List[Char], acc: Int): Boolean = {
+      if (chars.tail.isEmpty && chars.head == '(') false
+      else if (chars.tail.isEmpty && chars.head == ')' && acc == 0) false
+      else if (chars.tail.isEmpty && chars.head == ')' && acc == 1) true
+      else if (chars.tail.isEmpty && acc == 0) true
+      else if (chars.head == '(') maintainControl(chars.tail, acc + 1)
+      else if (chars.head == ')') maintainControl(chars.tail, acc - 1)
+      else maintainControl(chars.tail, acc)
+    }
+
+    maintainControl(chars, 0)
+  }
 
   /**
     * Exercise 3
